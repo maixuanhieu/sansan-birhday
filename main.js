c714183fb30ac93eb997ba9f0ca4ed923b782456
Hieu.Mai <xuanhieu26295@gmail.com>
2022-06-20 20:32:24 +0700

    up

---
+++ main.js
@@ -0,0 +1,14 @@
+const card = document.querySelector('.card__inner');
+
+card.addEventListener('click', function(){
+    card.classList.toggle('is-flipped');
+});
+
+// var x = document.getElementById("myAudio");
+// function Reset() {
+//     // highest = 0;
+//     x.play();
+// }
+
+  // control audio
+ 
